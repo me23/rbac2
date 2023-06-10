73de89472f089c58ac3c6a73c38d6a8a98f66b7c
Luis Lobo Borobia <luis.lobo@twenty20solutions.com>
2023-06-10 00:46:52 -0500

    Revert code to standard mocha format

--- test/rbac.async.spec.js
+++ test/rbac.async.spec.js
@@ -1,30 +1,177 @@
 const RBAC = require('..');
 const rules = require('./fixtures/rules');
 
-function rbacTestPromise(rbac) {
-  it('should return true when a path is found',
-    async () => rbac.check('admin', 'read articles').should.eventually.equal(true));
-  it('should return false when a path is not found',
-    async () => rbac.check('user', 'delete user').should.eventually.equal(false));
-  it('should return false when a conditional is not satisfied',
-    async () => rbac.check('user', 'edit article').should.eventually.equal(false));
-  it('should return false when a conditional is not satisfied',
-    async () => rbac.check('user', 'edit article', { userId: 1 }).should.eventually.equal(false));
-  it('should work when a callback conditional is satisfied',
-    async () => rbac.check('user', 'edit article', { userId: 2 }).should.eventually.equal(true));
-  it('should work an async conditional is satisfied',
-    async () => rbac.check('user', 'delete article', { userId: 3 }).should.eventually.equal(true));
+describe('RBAC', () => {
+  let rbac;
 
-  it('should allow a higher node to ignore conditionals based on "checkFullPath"',
-    async () => rbac.check('superadmin', 'delete article').should.eventually.equal(!rbac.checkFullPath));
+  describe('w/o caching (promises/async/await)', () => {
+    before(() => {
+      rbac = new RBAC(rules);
+    });
 
-  it('should propagate conditional errors from callbacks',
-    async () => rbac.check('user', 'do nothing with callbacks').should.eventually.be.rejected);
-  it('should propagate conditional errors from promises',
-    async () => rbac.check('user', 'do nothing with promises').should.eventually.be.rejected);
-}
+    it('should return true when a path is found', async () => {
+      const result = await rbac.check('admin', 'read articles');
+      should.equal(result, true);
+    });
 
+    it('should return false when a path is not found', async () => {
+      const result = await rbac.check('user', 'delete user');
+      should.equal(result, false);
+    });
 
-describe('RBAC w/o caching (promises/async/await)', () => rbacTestPromise(new RBAC(rules)));
-describe('RBAC when checking full trees (promises/async/await)', () => rbacTestPromise(new RBAC(rules, true, true)));
-describe('RBAC w/caching (promises/async/await)', () => rbacTestPromise(new RBAC(rules, false, true)));
+    it('should return false when a conditional is not satisfied', async () => {
+      const result = await rbac.check('user', 'edit article');
+      should.equal(result, false);
+    });
+
+    it('should return false when a conditional is not satisfied w/params', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 1 });
+      should.equal(result, false);
+    });
+
+    it('should work when a callback conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 2 });
+      should.equal(result, true);
+    });
+
+    it('should work an async conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'delete article', { userId: 3 });
+      should.equal(result, true);
+    });
+
+    it('should allow a higher node to ignore conditionals based on "checkFullPath"', async () => {
+      const result = await rbac.check('superadmin', 'delete article');
+      should.equal(result, !rbac.checkFullPath);
+    });
+
+    it('should propagate conditional errors from callbacks', async () => {
+      try {
+        await rbac.check('user', 'do nothing with callbacks');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+
+    it('should propagate conditional errors from promises', async () => {
+      try {
+        await rbac.check('user', 'do nothing with promises');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+  });
+
+  describe('when checking full trees (promises/async/await)', () => {
+    before(() => {
+      rbac = new RBAC(rules, true, true);
+    });
+
+    it('should return true when a path is found', async () => {
+      const result = await rbac.check('admin', 'read articles');
+      should.equal(result, true);
+    });
+
+    it('should return false when a path is not found', async () => {
+      const result = await rbac.check('user', 'delete user');
+      should.equal(result, false);
+    });
+
+    it('should return false when a conditional is not satisfied', async () => {
+      const result = await rbac.check('user', 'edit article');
+      should.equal(result, false);
+    });
+
+    it('should return false when a conditional is not satisfied w/params', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 1 });
+      should.equal(result, false);
+    });
+
+    it('should work when a callback conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 2 });
+      should.equal(result, true);
+    });
+
+    it('should work an async conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'delete article', { userId: 3 });
+      should.equal(result, true);
+    });
+
+    it('should allow a higher node to ignore conditionals based on "checkFullPath"', async () => {
+      const result = await rbac.check('superadmin', 'delete article');
+      should.equal(result, !rbac.checkFullPath);
+    });
+
+    it('should propagate conditional errors from callbacks', async () => {
+      try {
+        await rbac.check('user', 'do nothing with callbacks');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+
+    it('should propagate conditional errors from promises', async () => {
+      try {
+        await rbac.check('user', 'do nothing with promises');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+  });
+
+  describe('w/caching (promises/async/await)', () => {
+    before(() => {
+      rbac = new RBAC(rules, false, true);
+    });
+
+    it('should return true when a path is found', async () => {
+      const result = await rbac.check('admin', 'read articles');
+      should.equal(result, true);
+    });
+
+    it('should return false when a path is not found', async () => {
+      const result = await rbac.check('user', 'delete user');
+      should.equal(result, false);
+    });
+
+    it('should return false when a conditional is not satisfied', async () => {
+      const result = await rbac.check('user', 'edit article');
+      should.equal(result, false);
+    });
+
+    it('should return false when a conditional is not satisfied w/params', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 1 });
+      should.equal(result, false);
+    });
+
+    it('should work when a callback conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'edit article', { userId: 2 });
+      should.equal(result, true);
+    });
+
+    it('should work an async conditional is satisfied', async () => {
+      const result = await rbac.check('user', 'delete article', { userId: 3 });
+      should.equal(result, true);
+    });
+
+    it('should allow a higher node to ignore conditionals based on "checkFullPath"', async () => {
+      const result = await rbac.check('superadmin', 'delete article');
+      should.equal(result, !rbac.checkFullPath);
+    });
+
+    it('should propagate conditional errors from callbacks', async () => {
+      try {
+        await rbac.check('user', 'do nothing with callbacks');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+
+    it('should propagate conditional errors from promises', async () => {
+      try {
+        await rbac.check('user', 'do nothing with promises');
+      } catch (err) {
+        should.exist(err);
+      }
+    });
+  });
+});
